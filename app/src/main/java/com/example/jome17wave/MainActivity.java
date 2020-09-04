534b6737a5d4d1507989580126e98dff07f4fc8c
JB <jambing78@gmail.com>
2020-09-04 22:03:10 +0800

    Revert "drawable discolorIcon"
    This reverts commit a82052348810a27cfc41e3d7c9e2a493aab6ede3.

--- app/src/main/java/com/example/jome17wave/MainActivity.java
+++ app/src/main/java/com/example/jome17wave/MainActivity.java
@@ -1,16 +1,8 @@
 package com.example.jome17wave;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.NavHostController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
-
-import com.google.android.material.bottomnavigation.BottomNavigationItemView;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.internal.NavigationMenuItemView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,8 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
-        NavController navController = Navigation.findNavController(this, R.id.fragment);
-        NavigationUI.setupWithNavController(bottomNavigationView,navController);
     }
 }
