a82052348810a27cfc41e3d7c9e2a493aab6ede3
JB <jambing78@gmail.com>
2020-09-04 22:01:46 +0800

    drawable discolorIcon

--- app/src/main/java/com/example/jome17wave/MainActivity.java
+++ app/src/main/java/com/example/jome17wave/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.jome17wave;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.NavHostController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.internal.NavigationMenuItemView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavController navController = Navigation.findNavController(this, R.id.fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView,navController);
     }
 }
