e3c49540dd0b57d0945e076c9db42306c723eb70
Karena <Karenchung1201@gmail.com>
2020-09-07 13:27:01 +0800

    update trash code

--- app/src/main/java/com/example/jome17wave/main/mainFragment.java
+++ app/src/main/java/com/example/jome17wave/main/mainFragment.java
@@ -6,10 +6,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-<<<<<<< HEAD
-import androidx.navigation.NavController;
-=======
->>>>>>> Karena
+
 import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
