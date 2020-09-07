b7fa7c24b9711ab5b196e9acc6804732951bddbd
Karena <Karenchung1201@gmail.com>
2020-09-07 12:00:46 +0800

    新增Layout們、還有notification裡的icon

--- app/src/main/java/com/example/jome17wave/main/mainFragment.java
+++ app/src/main/java/com/example/jome17wave/main/mainFragment.java
@@ -2,7 +2,10 @@ package com.example.jome17wave.main;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
