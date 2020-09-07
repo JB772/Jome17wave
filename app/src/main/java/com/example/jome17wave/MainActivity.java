6e6286e7b097d0b2544ceb7694f1d37454f66042
Karena <Karenchung1201@gmail.com>
2020-09-07 14:47:20 +0800

    toolBar

--- app/src/main/java/com/example/jome17wave/MainActivity.java
+++ app/src/main/java/com/example/jome17wave/MainActivity.java
@@ -5,27 +5,22 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
-<<<<<<< HEAD
+
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
 import android.app.Activity;
-=======
+
 import androidx.navigation.NavDestination;
 import androidx.navigation.NavHostController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
 import android.graphics.drawable.ColorDrawable;
->>>>>>> justin_branch
+
 import android.os.Bundle;
 
-<<<<<<< HEAD
-=======
 
-import com.google.android.material.bottomnavigation.BottomNavigationItemView;
-
->>>>>>> 08bd34064e6b161e5729222300dcec74f21cdb4e
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
@@ -40,25 +35,7 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         NavController navController = Navigation.findNavController(this, R.id.fragment);
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
-<<<<<<< HEAD
-=======
 
-//        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
-//        NavController navController = Navigation.findNavController(this, R.id.fragment);
-//        NavigationUI.setupWithNavController(bottomNavigationView, navController);
-
-
-=======
-        //設定BottomBar
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
-        NavController navController = Navigation.findNavController(this, R.id.fragment);
-        NavigationUI.setupWithNavController(bottomNavigationView,navController);
-
-//        //設定appBar
-//        ActionBar actionBar = getSupportActionBar();
-//        actionBar.setDisplayHomeAsUpEnabled(true);
->>>>>>> justin_branch
->>>>>>> 08bd34064e6b161e5729222300dcec74f21cdb4e
 
     }
 }
