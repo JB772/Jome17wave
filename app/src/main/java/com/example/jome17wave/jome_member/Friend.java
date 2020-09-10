05932f47aab579867d5d65462eb3334acb65af99
JB <jambing78@gmail.com>
2020-09-10 10:05:12 +0800

    item_view_member_friend

---
+++ app/src/main/java/com/example/jome17wave/jome_member/Friend.java
@@ -0,0 +1,41 @@
+package com.example.jome17wave.jome_member;
+
+public class Friend {
+    //    private Bitmap imageFriend;
+    private int imageFriendId;
+    private String nameFriend;
+
+    public Friend() {
+    }
+
+    public Friend(int imageFriendId, String nameFriend) {
+        this.imageFriendId = imageFriendId;
+        this.nameFriend = nameFriend;
+    }
+
+    //    public Bitmap getImageFriend() {
+//        return imageFriend;
+//    }
+//
+//    public void setImageFriend(Bitmap imageFriend) {
+//        this.imageFriend = imageFriend;
+//    }
+
+
+    public int getImageFriendId() {
+        return imageFriendId;
+    }
+
+    public void setImageFriendId(int imageFriendId) {
+        this.imageFriendId = imageFriendId;
+    }
+
+    public String getNameFriend() {
+        return nameFriend;
+    }
+
+    public void setNameFriend(String nameFriend) {
+        this.nameFriend = nameFriend;
+    }
+
+}
