7d2818879a6d0d05bf09c949023b1e7eeca0f3af
scottshih <scott.big@yahoo.com.tw>
2020-09-23 17:44:31 +0800

    新增揪團頁面按鈕功能、新增圖片

---
+++ app/src/main/java/com/example/jome17wave/jome_group/GroupInfoFragment.java
@@ -0,0 +1,40 @@
+package com.example.jome17wave.jome_group;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.jome17wave.R;
+
+
+public class GroupInfoFragment extends Fragment {
+
+
+
+
+
+
+    public GroupInfoFragment() {
+        // Required empty public constructor
+    }
+
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_group_info, container, false);
+    }
+}
