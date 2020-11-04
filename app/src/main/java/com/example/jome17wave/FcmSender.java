52c276545ebeb46d81fb7787bb105b3ce4a2bd89
JB <jambing78@gmail.com>
2020-11-04 18:35:46 +0800

    update

--- app/src/main/java/com/example/jome17wave/FcmSender.java
+++ app/src/main/java/com/example/jome17wave/FcmSender.java
@@ -1,20 +1,28 @@
 package com.example.jome17wave;
 
 import android.content.Context;
+import android.util.Log;
 
 import com.example.jome17wave.jome_Bean.FriendListBean;
 import com.example.jome17wave.jome_Bean.JomeMember;
+import com.example.jome17wave.jome_Bean.PersonalGroupBean;
 import com.example.jome17wave.task.CommonTask;
 import com.google.gson.JsonObject;
 
-public class FcmSender {
+import java.util.concurrent.ExecutionException;
 
+public class FcmSender {
+    private static final String TAG = "FcmSender: ";
+    private final String url = Common.URL_SERVER + "jome_member/FcmBasicServlet";
+    private String titleStr = "";
+    private String bodyStr = "";
+    private String dataStr = "";
+    private CommonTask fcmTask;
     /**
      * 將registration token傳送至server
      */
     public void sendTokenToServer(String token, Context context) {
         if (Common.networkConnected(context)) {
-            String url = Common.URL_SERVER + "jome_member/FcmBasicServlet";
             JsonObject jsonObject = new JsonObject();
             jsonObject.addProperty("action", "register");
             jsonObject.addProperty("registrationToken", token);
@@ -31,27 +39,54 @@ public class FcmSender {
      */
     public void friendFcmSender(Context context, FriendListBean relationBean){
         if (Common.networkConnected(context)){
-            String url = Common.URL_SERVER + "jome_member/FcmBasicServlet";
             JomeMember mainMember = Common.getSelfFromPreference(context);
             JsonObject jsonObject = new JsonObject();
             jsonObject.addProperty("action", "singleFcm");
-            jsonObject.addProperty("title", "好友通知");
+            titleStr = "好友通知";
             if (mainMember.getMember_id().equals(relationBean.getInvite_M_ID())){
-                String bodyStr = " ";
                 jsonObject.addProperty("memberId", relationBean.getAccept_M_ID());
                 switch (relationBean.getFriend_Status()){
                     case 1:
                         bodyStr = mainMember.getNickname() + "已同意你的好友邀請";
+                        dataStr = "messageCenter";
                         break;
 
                     case 3:
                         bodyStr = mainMember.getNickname() + "發送好友邀請給你";
+                        dataStr = "messageCenter";
                         break;
+
+                    default:
+                        return;
                 }
-                jsonObject.addProperty("body", bodyStr);
+            }else if(mainMember.getMember_id().equals(relationBean.getAccept_M_ID())){
+                jsonObject.addProperty("memberId", relationBean.getInvite_M_ID());
+                switch (relationBean.getFriend_Status()){
+                    case 1:
+                        bodyStr = mainMember.getNickname() + "已同意你的好友邀請";
+                        dataStr = "messageCenter";
+                        break;
+                    case 3:
+                        bodyStr = mainMember.getNickname() + "發送好友邀請給你";
+                        dataStr = "messageCenter";
+                        break;
+
+                    default:
+                        return;
+                }
             }
-
-
+            jsonObject.addProperty("title", titleStr);
+            jsonObject.addProperty("body", bodyStr);
+            jsonObject.addProperty("data", dataStr);
+            fcmTask = new CommonTask(url, jsonObject.toString());
+            try {
+                String senderResult = fcmTask.execute().get();
+                Log.d(TAG, senderResult);
+            } catch (ExecutionException e) {
+                Log.e(TAG, e.toString());
+            } catch (InterruptedException e) {
+                Log.e(TAG, e.toString());
+            }
 
 
         }
@@ -60,8 +95,13 @@ public class FcmSender {
     /**
      * 揪團Fcm
      */
-    public void groupFcmSender(){
+    public void groupFcmSender(Context context, PersonalGroupBean perGroupBean){
+        if(Common.networkConnected(context)){
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("action", "singleFcm");
+            titleStr = "揪團通知";
 
+        }
     }
 
     /**
