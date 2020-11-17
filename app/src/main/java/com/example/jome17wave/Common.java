4e5c65ee8c750748f78fe1b55eeab520449fdca3
JB <jambing78@gmail.com>
2020-11-17 16:59:20 +0800

    fix merge

--- app/src/main/java/com/example/jome17wave/Common.java
+++ app/src/main/java/com/example/jome17wave/Common.java
@@ -123,7 +123,7 @@ public class Common {
         Date assembleTime = str2Date(assembleStr);
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(assembleTime);
-        calendar.add(Calendar.MINUTE, -3);
+        calendar.add(Calendar.MINUTE, -1);
         return date2StrHm(calendar.getTime());
     }
 
